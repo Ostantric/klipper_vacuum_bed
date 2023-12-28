04a20ab5f63cb2d1600c10defc8e1dde98114f39
Murat <murat.terzi01@gmail.com>
2023-12-27 20:19:57 -0500

    timer fix

--- klippy/vacuum.py
+++ klippy/vacuum.py
@@ -3,7 +3,7 @@ from . import bus
 from struct import unpack_from
 
 
-REPORT_TIME = 2
+VACUUM_REPORT_TIME = 1.25
 ADC_REPORT_TIME = 0.5
 ADC_SAMPLE_TIME = 0.001
 ADC_SAMPLE_COUNT = 6
@@ -23,6 +23,8 @@ class vacuum:
         self.minimum_vacuum = config.getfloat('minimum_vac')
         self.maximum_vacuum = config.getfloat('maximum_vac')
         self.vacuum_level = -1
+        self.vacuum_level_abs = 0
+        self.sample_timer = None
         self.is_system_activate = False
         self.is_system_running = False
         max_mcu_duration = config.getfloat('maximum_mcu_duration', 0.,
@@ -61,8 +63,14 @@ class vacuum:
             desc=self.cmd_DISABLE_VACUUM_help)
         gcode.register_command('EMPTY_VACUUM_TANK', self.cmd_EMPTY_VACUUM_TANK,
             desc=self.cmd_EMPTY_VACUUM_TANK_help)
+        self.printer.register_event_handler(
+            "klippy:connect", self.vacuumcheck_loop_start)
 
-    
+
+    def vacuumcheck_loop_start(self):
+        logging.info("Vacuum loop init")
+        self.sample_timer = self.reactor.register_timer(self.check_vacuum_level)
+        self.reactor.update_timer(self.sample_timer, self.reactor.NOW)
     def cmd_ENABLE_VACUUM(self,gcmd):
         self.is_system_activate=True
         self.is_system_running=False
@@ -82,39 +90,44 @@ class vacuum:
     #Turn on sequence
     def _turn_on(self, print_time):
         self.mcu_motor_pin.set_digital(print_time, 1)
-        self.mcu_valve_pin.set_digital(print_time + 1, 1) #add 1 second in between
+        self.mcu_valve_pin.set_digital(print_time + 2, 1) #add 2 seconds in between
     #Turn off sequence
     def _turn_off(self,print_time):
         self.mcu_valve_pin.set_digital(print_time, 0)
-        #self.reactor.pause(self.reactor.monotonic() + 2) # Bad
-        self.mcu_motor_pin.set_digital(print_time + 1, 0) # add 1 second in between
+        #self.reactor.pause(self.reactor.monotonic() + 2) # Bad,don't do this
+        self.mcu_motor_pin.set_digital(print_time + 2, 0) # add 2 seconds in between
     #Repressure Tank sequence
     def _empty_tank(self,print_time):
         self.mcu_motor_pin.set_digital(print_time, 0)
-        self.mcu_valve_pin.set_digital(print_time + 1, 1)
-
+        self.mcu_valve_pin.set_digital(print_time + 2, 1) # add 2 seconds in between
+    def setup_callback(self, cb):
+        self._callback = cb
 
     def adc_callback(self, read_time, read_value):
         #adc = max(.00001, min(.99999, read_value))
         #self.vacuum_level = adc / (1.0 - adc)
+        #self.vacuum_level = ((((read_value - 0.00001) * (47.8 - 0)) / (0.99999 - 0.00001)) +0) - 31
         self.vacuum_level = ((((read_value - 0.1) * (30 - 0)) / (0.99999 - 0.1)) +0)
-        vacuum_level_abs = abs(self.vacuum_level)
+        self.vacuum_level_abs = abs(self.vacuum_level)
+
+    def check_vacuum_level(self,eventtime):
         if self.is_system_activate:
-            if(vacuum_level_abs <= self.minimum_vacuum):
+            if(self.vacuum_level_abs < self.minimum_vacuum):
                 if not self.is_system_running:
                     toolhead = self.printer.lookup_object('toolhead')
                     toolhead.register_lookahead_callback(
                     lambda print_time: self._turn_on(print_time))
                     #logging.info("Vacuum is LOW, system POWER On")
                     self.is_system_running=True   
-            else:
-                if (vacuum_level_abs >= self.maximum_vacuum):
+            if(self.vacuum_level_abs > self.maximum_vacuum):
                     if self.is_system_running:
                         toolhead = self.printer.lookup_object('toolhead')
                         toolhead.register_lookahead_callback(
                         lambda print_time: self._turn_off(print_time))
                         #logging.info("Vacuum is High, system POWER Off")
                         self.is_system_running=False
+        measured_time = self.reactor.monotonic()
+        return measured_time + VACUUM_REPORT_TIME
     
     def get_status(self, eventtime):
         data = {'vacuum': self.vacuum_level}
@@ -122,4 +135,3 @@ class vacuum:
 
 def load_config_prefix(config):
     return vacuum(config)
-        
